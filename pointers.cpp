0795ec9fb3b4df8c4cd923ead1fa20b21d170e66
Palumbo <palu3492@STTHOMAS.EDU>
2020-02-06 13:28:55 -0600

    Added user input

--- pointers.cpp
+++ pointers.cpp
@@ -19,34 +19,71 @@ int main(int argc, char **argv) {
 
     // Sequence of user input -> store in fields of `student`
     
-    student.id = 123456;
+	// Student ID
+    //student.id = 123456;
+	std::cout << "Please enter the student's id number: ";
+	std::cin >> student.id;
     
+	// First name
     student.f_name = new char[20];
+	std::cout << "Please enter the student's first name: ";
+	std::cin >> student.f_name;
+	
+	// Last name
     student.l_name = new char[20];
-    strcpy(student.f_name, "Jane");
-    strcpy(student.l_name, "Doe");
+	std::cout << "Please enter the student's last name: ";
+	std::cin >> student.l_name;
+	
+    // strcpy(student.f_name, "Jane");
+    // strcpy(student.l_name, "Doe");
     
-    student.n_assignments = 4;
+    // student.n_assignments = 4;
+	// Number of assignments
+	std::cout << "Please enter how many assignments were graded: ";
+	std::cin >> student.n_assignments;
+	std::cout << std::endl;
     
-    student.grades = new double[student.n_assignments];
+	
+	// Grades of assignments
+	student.grades = new double[student.n_assignments];
+	for(int i = 0; i < student.n_assignments; i++){
+		std::cout << "Please enter grade for assignment " << i << ": ";
+		std::cin >> student.grades[i];
+	}
+    /*
     student.grades[0] = 98.02;
     student.grades[1] = 59.98;
     student.grades[2] = 76.28;
     student.grades[3] = 85.99;
-
+	*/
+	/*
     printf("id: %d, first: %s, last: %s \n", student.id, student.f_name, student.l_name);
     printf("assingments: %d \n", student.n_assignments);
     printf("grades: \n");
     for(int i = 0; i < student.n_assignments; i++){
     	printf("  %.2f \n", student.grades[i]);
     }
-
-    // Call `CalculateStudentAverage(???, ???)`
-    // Output `average`
-
+	*/
+	
+	double avg = 0.0;
+	// send pointer to avg
+	calculateStudentAverage(student.grades, &avg);
+	// avg should now contain the average of the grades
+	std::cout << std::endl;
+    std::cout << "Student: " << student.f_name << " " << student.l_name << "[" << student.id << "]";
+	std::cout << std::endl;
+	std::cout << "  Average grade: " << avg;
     return 0;
 }
 
+// Function to calculate and store average grade
+// takes in pointer to avg (double)
+
 void calculateStudentAverage(void *object, double *avg) {
-    // Code to calculate and store average grade
+	double total = 0.0;
+	for(int i = 0; i < object->size(); i++){
+    	total += object[i];
+    }
+	*avg = total/(object->size());
 }
+
