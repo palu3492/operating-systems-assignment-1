b764b81e7fd4a21dc2cedb8ead163076a3af61f3
Alex Palumbo <palu3492@stthomas.edu>
2020-02-05 19:52:51 -0600

    Started assignment

--- pointers.cpp
+++ pointers.cpp
@@ -1,6 +1,7 @@
 // compile: g++ -std=c++11 -o pointers pointers.cpp
 #include <iostream>
 #include <string>
+#include <cstring>
 
 typedef struct Student {
     int id;
@@ -12,12 +13,33 @@ typedef struct Student {
 
 void calculateStudentAverage(void *object, double *avg);
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     Student student;
     double average;
 
     // Sequence of user input -> store in fields of `student`
+    
+    student.id = 123456;
+    
+    student.f_name = new char[20];
+    student.l_name = new char[20];
+    strcpy(student.f_name, "Jane");
+    strcpy(student.l_name, "Doe");
+    
+    student.n_assignments = 4;
+    
+    student.grades = new double[student.n_assignments];
+    student.grades[0] = 98.02;
+    student.grades[1] = 59.98;
+    student.grades[2] = 76.28;
+    student.grades[3] = 85.99;
+
+    printf("id: %d, first: %s, last: %s \n", student.id, student.f_name, student.l_name);
+    printf("assingments: %d \n", student.n_assignments);
+    printf("grades: \n");
+    for(int i = 0; i < student.n_assignments; i++){
+    	printf("  %.2f \n", student.grades[i]);
+    }
 
     // Call `CalculateStudentAverage(???, ???)`
     // Output `average`
@@ -25,7 +47,6 @@ int main(int argc, char **argv)
     return 0;
 }
 
-void calculateStudentAverage(void *object, double *avg)
-{
+void calculateStudentAverage(void *object, double *avg) {
     // Code to calculate and store average grade
 }
