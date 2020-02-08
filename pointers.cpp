9392e5f7650be7e6bb558890989efb9500fcbc10
Alex Palumbo <palu3492@stthomas.edu>
2020-02-08 11:52:05 -0600

    Update pointers.cpp
    Program working for taking user input. Need to do error checking and allow option to read and write files

--- pointers.cpp
+++ pointers.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <iomanip>
 
 typedef struct Student {
     int id;
@@ -20,7 +21,6 @@ int main(int argc, char **argv) {
     // Sequence of user input -> store in fields of `student`
     
 	// Student ID
-    //student.id = 123456;
 	std::cout << "Please enter the student's id number: ";
 	std::cin >> student.id;
     
@@ -33,57 +33,39 @@ int main(int argc, char **argv) {
     student.l_name = new char[20];
 	std::cout << "Please enter the student's last name: ";
 	std::cin >> student.l_name;
-	
-    // strcpy(student.f_name, "Jane");
-    // strcpy(student.l_name, "Doe");
     
-    // student.n_assignments = 4;
 	// Number of assignments
 	std::cout << "Please enter how many assignments were graded: ";
 	std::cin >> student.n_assignments;
 	std::cout << std::endl;
     
-	
 	// Grades of assignments
 	student.grades = new double[student.n_assignments];
 	for(int i = 0; i < student.n_assignments; i++){
 		std::cout << "Please enter grade for assignment " << i << ": ";
 		std::cin >> student.grades[i];
 	}
-    /*
-    student.grades[0] = 98.02;
-    student.grades[1] = 59.98;
-    student.grades[2] = 76.28;
-    student.grades[3] = 85.99;
-	*/
-	/*
-    printf("id: %d, first: %s, last: %s \n", student.id, student.f_name, student.l_name);
-    printf("assingments: %d \n", student.n_assignments);
-    printf("grades: \n");
-    for(int i = 0; i < student.n_assignments; i++){
-    	printf("  %.2f \n", student.grades[i]);
-    }
-	*/
 	
 	double avg = 0.0;
-	// send pointer to avg
-	calculateStudentAverage(student.grades, &avg);
+	// send pointer student and avg
+	calculateStudentAverage(&student, &avg);
 	// avg should now contain the average of the grades
 	std::cout << std::endl;
     std::cout << "Student: " << student.f_name << " " << student.l_name << "[" << student.id << "]";
 	std::cout << std::endl;
-	std::cout << "  Average grade: " << avg;
+	std::cout << "  Average grade: " << std::fixed << std::setprecision(1) << avg;
+	std::cout << std::endl;
     return 0;
 }
 
 // Function to calculate and store average grade
-// takes in pointer to avg (double)
-
+// Takes in pointer to <Student>student and <double>avg
 void calculateStudentAverage(void *object, double *avg) {
 	double total = 0.0;
-	for(int i = 0; i < object->size(); i++){
-    	total += object[i];
+	double size = ((Student*) object)->n_assignments;
+	for(int i = 0; i < size; i++){
+    	total += ((Student*) object)->grades[i];
     }
-	*avg = total/(object->size());
+	*avg = total/size;
 }
 
