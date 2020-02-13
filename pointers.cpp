0fa9c93c1b44b6da34b9531a1dedea4555c67295
Alex Palumbo <palu3492@stthomas.edu>
2020-02-12 21:08:08 -0600

    Update pointers.cpp
    Cleaned up, everything working

--- pointers.cpp
+++ pointers.cpp
@@ -7,7 +7,7 @@
 // #include<limits>
 #include<regex>
 
-using namespace std; // allows omitting  in front of cin
+using namespace std; // allows omitting std::
 
 typedef struct Student {
     int id;
@@ -26,76 +26,43 @@ int main(int argc, char **argv) {
 
     // Sequence of user input -> store in fields of `student`
     
-	// Student ID
-	// Non-negative number
-	/*
-	do{
-		cout << "Please enter the student's id number: ";
-		cin >> student.id;
-		if(student.id < 0) {
-			cout << "Sorry, I cannot understand your answer" << endl;
-		}
-	}while(student.id < 0);
-	*/
-	double id = 0;
+	// Student ID (non-negative)
+	double id = -1;
 	getUserInput("Please enter the student's id number: ", 0, &id);
 	student.id = id;
     
 	// First name
-    student.f_name = new char[10];
+    student.f_name = new char[128];
 	cout << "Please enter the student's first name: ";
 	cin >> student.f_name;
-	if(regex_search(student.f_name, regex("\\n+"))){
-		cout << endl << "newline" << endl;
-	}
 	
 	// Last name
-    student.l_name = new char[10];
+    student.l_name = new char[128];
 	cout << "Please enter the student's last name: ";
 	cin >> student.l_name;
     
-	// Number of assignments, n >= 1
-	/*
-	student.n_assignments = 0;
-	while(student.n_assignments < 1) {
-		cout << "Please enter how many assignments were graded: ";
-		cin >> student.n_assignments; // 'A6' error
-		if(student.n_assignments < 1) {
-			cout << "Sorry, I cannot understand your answer" << endl;
-		}
-	}
-	*/
-	double n_assignments = 0;
+	// Number of assignments (positive)
+	double n_assignments = -1;
 	getUserInput("Please enter how many assignments were graded: ", 1, &n_assignments);
 	student.n_assignments = n_assignments;
 	
 	cout << endl; // keep this
     
-	// Grades of assignments
-	// Non-negative number
+	// Grades of assignments (non-negative)
 	student.grades = new double[student.n_assignments];
 	for(int i = 0; i < student.n_assignments; i++) {
-		// student.grades[i] = -1;
+		student.grades[i] = -1;
 		string msg = "Please enter grade for assignment " + to_string(i) + ": ";
 		getUserInput(msg, 0, &student.grades[i]);
-		/*
-		while(student.grades[i] < 0) {
-			/*
-			cout << "Please enter grade for assignment " << i << ": ";
-			cin >> student.grades[i];
-			if(student.grades[i] < 0) {
-				cout << "Sorry, I cannot understand your answer" << endl;
-			}
-			
-			//getUserInput("Please enter grade for assignment i: ", 0, &student.grades[i]);
-		}
-		*/
 	}
 	
+	// Average of assignments
 	double avg = 0.0;
-	// send pointer student and avg
+	// send pointer to <Student>student and <double>avg
 	calculateStudentAverage(&student, &avg);
 	// avg should now contain the average of the grades
+	
+	// Print everything
 	cout << endl;
     cout << "Student: " << student.f_name << " " << student.l_name << " [" << student.id << "]";
 	cout << endl;
@@ -115,32 +82,21 @@ void calculateStudentAverage(void *object, double *avg) {
 	*avg = total/size;
 }
 
-
+// Function to take user input and error check
 void getUserInput(string msg, int min, double *var){
-	string s = "";	
+	string input = "";	
 	do{
 		cout << msg;
-		cin >> s;
-		if(regex_search(s, regex("[^\\d.]+"))){
+		cin >> input;
+		if(regex_search(input, regex("[^\\d.]+"))){
 			cout << "Sorry, I cannot understand your answer" << endl;
 		} else {
-			stringstream stream(s);
+			stringstream stream(input);
 			stream >> *var;
-			break;
-		}
-	}while(true);
-	/*
-	do{
-		cout << msg;
-		cin >> *var;
-		if(cin.fail() || *var < min) {
-			cout << "Sorry, I cannot understand your answer" << endl;
-			cin.clear(); // clears the error on cin
-			//cin.ignore(100,'\n'); // ignores all characters inputted into the input stream, numeric_limits<streamsize>::max()
-			cin.ignore(numeric_limits<streamsize>::max(),'\n');
-			*var = -1;
+			if(*var < min){
+				cout << "Sorry, I cannot understand your answer" << endl;
+			}
 		}
 	}while(*var < min);
-	*/
 }
 
