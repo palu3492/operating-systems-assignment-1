4bdabb5209a72db4d51fe3812ffd890f4c2fb115
Alex Palumbo <palu3492@stthomas.edu>
2020-02-12 20:39:12 -0600

    Update pointers.cpp
    Error checking working, need to clean it up

--- pointers.cpp
+++ pointers.cpp
@@ -2,10 +2,12 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <sstream> 
 #include <iomanip>
-#include<limits>
+// #include<limits>
+#include<regex>
 
-using namespace std; // allows omitting std:: in front of std::cin
+using namespace std; // allows omitting  in front of cin
 
 typedef struct Student {
     int id;
@@ -16,7 +18,7 @@ typedef struct Student {
 } Student;
 
 void calculateStudentAverage(void *object, double *avg);
-void getUserInput(string msg, int min, void *var);
+void getUserInput(string msg, int min, double *var);
 
 int main(int argc, char **argv) {
     Student student;
@@ -28,59 +30,77 @@ int main(int argc, char **argv) {
 	// Non-negative number
 	/*
 	do{
-		std::cout << "Please enter the student's id number: ";
-		std::cin >> student.id;
+		cout << "Please enter the student's id number: ";
+		cin >> student.id;
 		if(student.id < 0) {
-			std::cout << "Sorry, I cannot understand your answer" << std::endl;
+			cout << "Sorry, I cannot understand your answer" << endl;
 		}
 	}while(student.id < 0);
 	*/
-	getUserInput("Please enter the student's id number: ", 0, &student.id);
+	double id = 0;
+	getUserInput("Please enter the student's id number: ", 0, &id);
+	student.id = id;
     
 	// First name
-    student.f_name = new char[20];
-	std::cout << "Please enter the student's first name: ";
-	std::cin >> student.f_name;
+    student.f_name = new char[10];
+	cout << "Please enter the student's first name: ";
+	cin >> student.f_name;
+	if(regex_search(student.f_name, regex("\\n+"))){
+		cout << endl << "newline" << endl;
+	}
 	
 	// Last name
-    student.l_name = new char[20];
-	std::cout << "Please enter the student's last name: ";
-	std::cin >> student.l_name;
+    student.l_name = new char[10];
+	cout << "Please enter the student's last name: ";
+	cin >> student.l_name;
     
-	// Number of assignments, n >= 1	
+	// Number of assignments, n >= 1
+	/*
 	student.n_assignments = 0;
 	while(student.n_assignments < 1) {
-		std::cout << "Please enter how many assignments were graded: ";
-		std::cin >> student.n_assignments; // 'A6' error
+		cout << "Please enter how many assignments were graded: ";
+		cin >> student.n_assignments; // 'A6' error
 		if(student.n_assignments < 1) {
-			std::cout << "Sorry, I cannot understand your answer" << std::endl;
+			cout << "Sorry, I cannot understand your answer" << endl;
 		}
 	}
+	*/
+	double n_assignments = 0;
+	getUserInput("Please enter how many assignments were graded: ", 1, &n_assignments);
+	student.n_assignments = n_assignments;
+	
+	cout << endl; // keep this
     
 	// Grades of assignments
 	// Non-negative number
 	student.grades = new double[student.n_assignments];
 	for(int i = 0; i < student.n_assignments; i++) {
-		student.grades[i] = -1;
+		// student.grades[i] = -1;
+		string msg = "Please enter grade for assignment " + to_string(i) + ": ";
+		getUserInput(msg, 0, &student.grades[i]);
+		/*
 		while(student.grades[i] < 0) {
-			std::cout << "Please enter grade for assignment " << i << ": ";
-			std::cin >> student.grades[i];
+			/*
+			cout << "Please enter grade for assignment " << i << ": ";
+			cin >> student.grades[i];
 			if(student.grades[i] < 0) {
-				std::cout << "Sorry, I cannot understand your answer" << std::endl;
+				cout << "Sorry, I cannot understand your answer" << endl;
 			}
+			
+			//getUserInput("Please enter grade for assignment i: ", 0, &student.grades[i]);
 		}
-	
+		*/
 	}
 	
 	double avg = 0.0;
 	// send pointer student and avg
 	calculateStudentAverage(&student, &avg);
 	// avg should now contain the average of the grades
-	std::cout << std::endl;
-    std::cout << "Student: " << student.f_name << " " << student.l_name << "[" << student.id << "]";
-	std::cout << std::endl;
-	std::cout << "  Average grade: " << std::fixed << std::setprecision(1) << avg;
-	std::cout << std::endl;
+	cout << endl;
+    cout << "Student: " << student.f_name << " " << student.l_name << " [" << student.id << "]";
+	cout << endl;
+	cout << "  Average grade: " << fixed << setprecision(1) << avg;
+	cout << endl;
     return 0;
 }
 
@@ -96,18 +116,31 @@ void calculateStudentAverage(void *object, double *avg) {
 }
 
 
-void getUserInput(string msg, int min, void *var){
-	bool exit = false;
+void getUserInput(string msg, int min, double *var){
+	string s = "";	
 	do{
 		cout << msg;
-		cin >> *((double*) var);
-		if(std::cin.fail() || *((double*) var) < min) {
-			std::cout << "Sorry, I cannot understand your answer" << std::endl;
-			cin.clear(); // clears the error on cin
-			cin.ignore(numeric_limits<streamsize>::max(),'\n'); // ignores all characters inputted into the input stream
+		cin >> s;
+		if(regex_search(s, regex("[^\\d.]+"))){
+			cout << "Sorry, I cannot understand your answer" << endl;
 		} else {
-			exit = true;
+			stringstream stream(s);
+			stream >> *var;
+			break;
 		}
-	}while(!exit);
+	}while(true);
+	/*
+	do{
+		cout << msg;
+		cin >> *var;
+		if(cin.fail() || *var < min) {
+			cout << "Sorry, I cannot understand your answer" << endl;
+			cin.clear(); // clears the error on cin
+			//cin.ignore(100,'\n'); // ignores all characters inputted into the input stream, numeric_limits<streamsize>::max()
+			cin.ignore(numeric_limits<streamsize>::max(),'\n');
+			*var = -1;
+		}
+	}while(*var < min);
+	*/
 }
 
